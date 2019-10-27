fa4e5565209c5db90698beb4657c0a5384a35858
mc <marcin_czekala@icloud.com>
2019-10-27 10:21:35 +0100

    create basic routes

---
+++ resources/views/createPage.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <section class="section-maker">
+            <select class="section-maker__select">
+            </select>
+            <input class="section-maker__name" placeholder="section name"/>
+            <input class="section-maker__content" placeholder="section content"/>
+            <button>add section</button>
+        </section>
+    </body>
+</html>
