297c44434281aa9e1ff9bfdf9304860face75e50
mc <marcin_czekala@icloud.com>
2019-12-23 00:29:31 +0100

    fix name mismatch

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,8 +7,8 @@
     <h1>All pages</h1>
     <ul>
         @foreach ($pages as $page)
-            <li> 
-                <a href="/admin/{$page->name}">{{ $page->name}}</button>
+            <li>
+                <a href="/admin/{$page->name}">{{ $page->title}}</button>
             </li>
         @endforeach
     </ul>
